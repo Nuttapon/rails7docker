9b09875d5d9c9691b44d7a49ccb25c41c2d13fbb
Nuttapon Achachotipong <nuttapon.a@linecorp.com>
2023-09-18 19:39:56 +0700

    feat(post.rb): add validation for post title to ensure presence and length constraints test(post_test.rb): add tests for post title validation to ensure correct functionality

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save post without title" do
+    post = Post.new
+    assert_not post.save
+  end
+
+  test "should save post with title" do
+    post = Post.new
+    post.title = "Test"
+    assert post.save
+  end
 end
