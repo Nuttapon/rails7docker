7b15a2becb25ffa5fc3b1d67371f8a2a31cf2184
Nuttapon Achachotipong <nuttapon.a@linecorp.com>
2023-09-18 19:59:39 +0700

    customize rubocop

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,14 +1,16 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  test "should not save post without title" do
+  test 'should not save post without title' do
     post = Post.new
     assert_not post.save
   end
 
-  test "should save post with title" do
+  test 'should save post with title' do
     post = Post.new
-    post.title = "Test"
+    post.title = 'Test'
     assert post.save
   end
 end
